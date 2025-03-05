c53c57522f0391b5d73dd79cc90e945a22ff44e7
assu-2000 <kayassurance@gmail.com>
2025-03-05 04:19:27 +0100

    update package.json file

--- __transpiled/index.js
+++ __transpiled/index.js
@@ -2,15 +2,26 @@
 
 require('source-map-support/register');
 var generatorReactSdk = require('@asyncapi/generator-react-sdk');
-var jsxRuntime = require('C:/Users/lenovo/AppData/Roaming/npm/node_modules/@asyncapi/cli/node_modules/react/cjs/react-jsx-runtime.production.min.js');
+var jsxRuntime = require('C:/Users/lenovo/AppData/Roaming/npm/node_modules/@asyncapi/cli/node_modules/@asyncapi/generator/node_modules/react/cjs/react-jsx-runtime.production.min.js');
 
 function index ({
   asyncapi
 }) {
-  //3
   return /*#__PURE__*/jsxRuntime.jsx(generatorReactSdk.File, {
     name: "client.py",
-    children: asyncapi.info().title()
+    children: `import paho.mqtt.client as mqtt
+
+mqttBroker = "test.mosquitto.org"
+
+class TemperatureServiceClient:
+    def __init__(self):
+        self.client = mqtt.Client()
+        self.client.connect(mqttBroker)
+
+
+    def sendTemperatureChange(self, id):
+        topic = "temperature/changed"
+        self.client.publish(topic, id)`
   });
 }
 
--- test/project/client.py
+++ test/project/client.py
@@ -1,1 +1,13 @@
-Temperature Service
+import paho.mqtt.client as mqtt
+
+mqttBroker = "test.mosquitto.org"
+
+class TemperatureServiceClient:
+    def __init__(self):
+        self.client = mqtt.Client()
+        self.client.connect(mqttBroker)
+
+
+    def sendTemperatureChange(self, id):
+        topic = "temperature/changed"
+        self.client.publish(topic, id)
