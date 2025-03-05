cf247d6f99051dad3d7144df588193d001bdcda5
assu-2000 <kayassurance@gmail.com>
2025-03-05 03:42:32 +0100

    feat:files generation

---
+++ __transpiled/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('source-map-support/register');
+var generatorReactSdk = require('@asyncapi/generator-react-sdk');
+var jsxRuntime = require('C:/Users/lenovo/AppData/Roaming/npm/node_modules/@asyncapi/cli/node_modules/react/cjs/react-jsx-runtime.production.min.js');
+
+function index ({
+  asyncapi
+}) {
+  //3
+  return /*#__PURE__*/jsxRuntime.jsx(generatorReactSdk.File, {
+    name: "client.py",
+    children: asyncapi.info().title()
+  });
+}
+
+module.exports = index;
+//# sourceMappingURL=index.js.map
